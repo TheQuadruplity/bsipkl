ffccbb255da5001c743af2522a438206dbe910f4
TheQuadruplity <thev.vandicco@gmail.com>
2022-03-04 23:34:43 +0700

    implementasi beban model

--- app/Controllers/Beban.php
+++ app/Controllers/Beban.php
@@ -2,9 +2,13 @@
 
 namespace App\Controllers;
 
+use App\Models\BebanModel;
+
 class Beban extends BaseController
 {
     public function index(){
-        $this->page('daftar_beban');
+        $model = new BebanModel();
+        $databeban = $model->findAll();
+        $this->page('daftar_beban', ['data' => $databeban]);
     }
 }
