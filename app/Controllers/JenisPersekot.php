070a7db2af42f8d01b081189606ab2a1d687cf75
TheQuadruplity <thev.vandicco@gmail.com>
2022-03-12 01:46:28 +0700

    crud jenis persekot

--- app/Controllers/JenisPersekot.php
+++ app/Controllers/JenisPersekot.php
@@ -12,4 +12,33 @@ class JenisPersekot extends BaseController
         $data = $model->findAll();
         $this->page('jenis_persekot', ['data' => $data]);
     }
+
+    public function save(){
+        if($this->request->getMethod() == 'post'){
+            $model = new JenisPersekotModel();
+            $model->save([
+                'nama' => $this->request->getPost('nama')
+            ]);
+        }
+
+        return redirect()->to(base_url().'/jenispersekot');
+    }
+
+    public function delete($nomor){
+        $model = new JenisPersekotModel();
+        $model->delete($nomor);
+
+        return redirect()->to(base_url().'/jenispersekot');
+    }
+
+    public function update(){
+        if($this->request->getMethod() == 'post'){
+            $model = new JenisPersekotModel();
+            $model->update($this->request->getPost('id'), [
+                'nama' => $this->request->getPost('nama')
+            ]);
+        }
+
+        return redirect()->to(base_url().'/jenispersekot');
+    }
 }
--- app/Views/pages/jenis_persekot.php
+++ app/Views/pages/jenis_persekot.php
@@ -2,7 +2,9 @@
 <h1 class="h3 mb-2 text-gray-800">Daftar Jenis Persekot</h1>
 <p class="mb-4">Berikut adalah database Jenis Persekot</p>
 
-<a href="#" class="btn btn-primary mb-3">Tambah Data</a>
+<button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal">
+  Tambah Data
+</button>
 
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
@@ -25,8 +27,8 @@
                         <td><?= esc($i+1) ?></td>
                         <td><?= esc($d['nama']) ?></td>
                         <td>
-                            <a href="#" class="btn btn-danger" data-id='<?= esc($d['id']) ?>'>Hapus</a>
-                            <a href="#" class="btn btn-warning" data-id='<?= esc($d['id']) ?>'>Edit</a>
+                        <a href="<?= base_url()?>/jenispersekot/delete/<?= esc($d['id']) ?>" class="btn btn-danger">Hapus</a>
+                            <button type="button" class="btn btn-warning edit" data-toggle="modal" data-target="#editModal" data-id='<?= esc($d['id']) ?>' data-nama='<?= esc($d['nama']) ?>'>Edit</button>
                         </td>
                     </tr>
                     <?php endforeach ?>
@@ -35,3 +37,58 @@
         </div>
     </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Tambah Jenis Persekot</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form action="<?= base_url() ?>/jenispersekot/save" method="POST">
+        <div class="form-group">
+            <label for="nama">Nama Jenis Persekot</label>
+            <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan nama jenis persekot" required>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Edit Jenis Persekot</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form action="<?= base_url() ?>/jenispersekot/update" method="POST">
+        <div class="form-group">
+            <input type="hidden" name="id" id="editid" value="">
+            <label for="nama">Nama Jenis Persekot</label>
+            <input type="text" class="form-control" id="editnama" name="nama" placeholder="Masukkan nama beban" required>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<script>$('.edit').click(function (e) { $('#editnama').val(this.dataset['nama']);$('#editid').val(this.dataset['id']);});</script>
