c47536a8539f634ffcc29316568108c257d431d0
TheQuadruplity <thev.vandicco@gmail.com>
2022-03-12 00:11:52 +0700

    insert, delete, edit beban

--- app/Controllers/Beban.php
+++ app/Controllers/Beban.php
@@ -9,6 +9,36 @@ class Beban extends BaseController
     public function index(){
         $model = new BebanModel();
         $data = $model->findAll();
+        unset($data[0]);
         $this->page('daftar_beban', ['data' => $data]);
+    }
+
+    public function save(){
+        if($this->request->getMethod() == 'post'){
+            $model = new BebanModel();
+            $model->save([
+                'nama' => $this->request->getPost('nama')
+            ]);
+        }
+
+        return redirect()->to(base_url().'/beban');
+    }
+
+    public function delete($nomor){
+        $model = new BebanModel();
+        $model->delete($nomor);
+
+        return redirect()->to(base_url().'/beban');
+    }
+
+    public function update(){
+        if($this->request->getMethod() == 'post'){
+            $model = new BebanModel();
+            $model->update($this->request->getPost('id'), [
+                'nama' => $this->request->getPost('nama')
+            ]);
+        }
+
+        return redirect()->to(base_url().'/beban');
     }
 }
--- app/Views/pages/daftar_beban.php
+++ app/Views/pages/daftar_beban.php
@@ -2,7 +2,9 @@
 <h1 class="h3 mb-2 text-gray-800">Daftar Beban</h1>
 <p class="mb-4">Berikut adalah database beban</p>
 
-<a href="#" class="btn btn-primary mb-3">Tambah Data</a>
+<button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#exampleModal">
+  Tambah Data
+</button>
 
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
@@ -22,11 +24,12 @@
                 <tbody>
                     <?php foreach($data as $i => $d): ?>
                     <tr>
-                        <td><?= esc($i+1) ?></td>
+                        <td><?= esc($i) ?></td>
                         <td><?= esc($d['nama']) ?></td>
                         <td>
-                            <a href="#" class="btn btn-danger" data-id='<?= esc($d['id']) ?>'>Hapus</a>
-                            <a href="#" class="btn btn-warning" data-id='<?= esc($d['id']) ?>'>Edit</a>
+                            <a href="<?= base_url()?>/beban/delete/<?= esc($d['id']) ?>" class="btn btn-danger">Hapus</a>
+                            <!-- <a href="#" class="btn btn-warning" data-id='<?= esc($d['id']) ?>'>Edit</a> -->
+                            <button type="button" class="btn btn-warning edit" data-toggle="modal" data-target="#editModal" data-id='<?= esc($d['id']) ?>' data-nama='<?= esc($d['nama']) ?>'>Edit</button>
                         </td>
                     </tr>
                     <?php endforeach ?>
@@ -35,3 +38,63 @@
         </div>
     </div>
 </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form action="<?= base_url() ?>/beban/save" method="POST">
+        <div class="form-group">
+            <label for="nama">Nama Beban</label>
+            <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan nama beban" required>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form action="<?= base_url() ?>/beban/update" method="POST">
+        <div class="form-group">
+            <input type="hidden" name="id" id="editid" value="">
+            <label for="nama">Nama Beban</label>
+            <input type="text" class="form-control" id="editnama" name="nama" placeholder="Masukkan nama beban" required>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<script>
+    $('.edit').click(function (e) { 
+        $('#editnama').val(this.dataset['nama']);
+        $('#editid').val(this.dataset['id']);
+    });
+</script>
