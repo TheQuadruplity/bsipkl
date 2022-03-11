4f387786ce32df2879a5564d9275a48ae5435af6
TheQuadruplity <thev.vandicco@gmail.com>
2022-03-11 17:38:32 +0700

    penyelesaian page

--- app/Controllers/Penyelesaian.php
+++ app/Controllers/Penyelesaian.php
@@ -2,9 +2,38 @@
 
 namespace App\Controllers;
 
+use App\Models\BebanModel;
+use App\Models\PenyelesaianModel;
+use App\Models\PersekotModel;
+use App\Models\RekeningModel;
+
 class Penyelesaian extends BaseController
 {
     public function index(){
-        $this->page('penyelesaian');
+        $persekotmodel = new PersekotModel();
+        $bebanmodel = new BebanModel();
+        $rekeningmodel = new RekeningModel();
+        $persekot = $persekotmodel->builder()->
+        select('id, narasi')->
+        where('sisa < 0')->
+        get()->getResultArray();
+        $beban = $bebanmodel->findAll();
+        $rekening = $rekeningmodel->findAll();
+        $this->page('penyelesaian', 
+        ['persekot' => $persekot, 'beban' => $beban, 'rekening' => $rekening]);
+    }
+
+    public function save(){
+        $model = new PenyelesaianModel();
+        $data = [
+            'beban' => $this->request->getPost('beban'), 
+            'jumlah' => $this->request->getPost('jumlah'), 
+            'rekening' => $this->request->getPost('rekening'), 
+            'persekot' => $this->request->getPost('persekot')
+        ];
+
+        $model->insert($data);
+
+        return redirect()->to(base_url().'/posbeban');
     }
 }
