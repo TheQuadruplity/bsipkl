5de5dc797335cceede44e3a2760a1420db63f3ea
TheQuadruplity <thev.vandicco@gmail.com>
2022-03-12 15:45:46 +0700

    judul penyelesaian fixed

--- app/Views/pages/penyelesaian.php
+++ app/Views/pages/penyelesaian.php
@@ -1,4 +1,4 @@
-<h1 class="h3 mb-2 text-gray-800">Form Input Pengambilan</h1>
+<h1 class="h3 mb-2 text-gray-800">Form Input Penyelesaian</h1>
 <!--p class="mb-4">Berikut adalah database Jenis Persekot</p-->
 
 <form action="<?= base_url() ?>/penyelesaian/save" method="POST">
