95cab10feafc64740df62cec9d9e7232ab1933f7
TheQuadruplity <thev.vandicco@gmail.com>
2022-03-14 14:35:40 +0700

    header/sidebar decor

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -8,7 +8,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2020</span>
+                        <span>Dibuat Oleh : Vandicco Maris Siregar &copy; 2021</span>
                     </div>
                 </div>
             </footer>
@@ -31,12 +31,12 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Logout</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body">Tekan Logout untuk mengakhiri sesi anda.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
                     <a class="btn btn-primary" href="<?= base_url('login/logout')?>">Logout</a>
