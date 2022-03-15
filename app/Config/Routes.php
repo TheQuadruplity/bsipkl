a99f86fc46081eca5be56af1f53b3e3b08f3e811
TheQuadruplity <thev.vandicco@gmail.com>
2022-03-16 01:13:44 +0700

    custom 404

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,7 +20,7 @@ $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Page');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
-$routes->set404Override();
+$routes->set404Override(function(){return view('404');});
 $routes->setAutoRoute(true);
 
 /*
