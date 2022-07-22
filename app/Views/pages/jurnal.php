3c76bd2349f03e108d5a83c929889bc4aa0f90cb
TheQuadruplity <thev.vandicco@gmail.com>
2022-07-22 07:33:35 +0700

    fixing view jurnal

--- app/Views/pages/jurnal.php
+++ app/Views/pages/jurnal.php
@@ -2,25 +2,24 @@
 <h1 class="h3 mb-2 text-gray-800">Jurnal</h1>
 <p class="mb-4">Berikut adalah jurnal untuk setiap bulan</p>
 
-<!-- Button trigger modal -->
-<div class="row">
-    <div class="col">
-        <label for="awal">Tanggal Awal</label>
-        <input type="date" class="form-control mb-3 tanggal" id="awal" min='<?=$datemin?>' max='<?=$datemax?>'  value="<?= $now?>">
-    </div>
-    <div class="col">
-        <label for="awal">Tanggal Akhir</label>
-        <input type="date" class="form-control mb-3 tanggal" id="akhir" min='<?=$datemin?>' max='<?=$datemax?>' value="<?= $now?>">
-    </div>
-</div>
-
-<a id='printb' target="_blank" href="<?= base_url("jurnal/print_j/$now/$now") ?>" class="btn btn-primary btn-icon-split mb-3"><span class="icon text-white-50">
-  <i class="fas fa-print"></i></span><span class="text">Cetak</span></a>
 
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Database</h6>
+        <!-- Button trigger modal -->
+        <div class="row">
+            <div class="col">
+                <label for="awal">Tanggal Awal</label>
+                <input type="date" class="form-control mb-3 tanggal" id="awal" min='<?=$datemin?>' max='<?=$datemax?>'  value="<?= $now?>">
+            </div>
+            <div class="col">
+                <label for="awal">Tanggal Akhir</label>
+                <input type="date" class="form-control mb-3 tanggal" id="akhir" min='<?=$datemin?>' max='<?=$datemax?>' value="<?= $now?>">
+            </div>
+        </div>
+
+        <a id='printb' target="_blank" href="<?= base_url("jurnal/print_j/$now/$now") ?>" class="btn btn-primary btn-icon-split"><span class="icon text-white-50">
+        <i class="fas fa-print"></i></span><span class="text">Cetak</span></a>
     </div>
     <div class="card-body">
         <div class="table-responsive">
